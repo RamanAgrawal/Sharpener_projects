e3968552e17f74dedfc64ec0730881d71a725824
RamanAgrawal <iramanagrawal@gmail.com>
2023-03-26 19:31:49 +0530

    making add and remove buttons working

--- Food-Order-App/src/components/Layout/Cart/Cart.jsx
+++ Food-Order-App/src/components/Layout/Cart/Cart.jsx
@@ -3,27 +3,41 @@ import Model from '../../UI/Model'
 import { useContext } from 'react'
 import FoodContext from '../../../store/FoodContext'
 import CartContext from '../../../store/CartContext'
+import CartItem from './CartItem'
 const Cart = props => {
-    const foodctx=useContext(FoodContext)
-    const cartCtx=useContext(CartContext)
-    const hasItem=cartCtx.items.length>0
-   
-    const cartItems = <ul>{cartCtx.items.map(item => (
-        <li key={item.id}>{item.name} {item.amount} x {item.price} = {item.amount*item.price}</li>
-        ))}</ul>
-        // console.log(cartItems)
+    const foodctx = useContext(FoodContext)
+    const cartCtx = useContext(CartContext)
+    const hasItem = cartCtx.items.length > 0
+    const cartItemRemoveHandler = (id) => {
+        cartCtx.removeItem(id)
+    }
+    const cartItemAddHandler = (item) => {
+
+        cartCtx.addItem({ ...item, amount: 1 })
+    }
+    const cartItems = <ul className={classes['cart-items']}>{cartCtx.items.map(item => (
+        <CartItem key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+        >
+        </CartItem>
+    ))}</ul>
+
     return <Model>
-    <div >
-        {cartItems}
-        <div className={classes.total}>
-            <span>Total Amount</span>
-            <span>{cartCtx.totalAmount}</span>
+        <div >
+            {cartItems}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>{cartCtx.totalAmount}</span>
+            </div>
+            <div className={classes.actions}>
+                <button onClick={foodctx.HideCartHandler}>Close</button>
+                {hasItem && <button>Order</button>}
+            </div>
         </div>
-        <div className={classes.actions}>
-            <button onClick={foodctx.HideCartHandler}>Close</button>
-           {hasItem&& <button>Order</button>}
-        </div>
-    </div>
     </Model>
 }
 
--- Food-Order-App/src/store/CartProvider.jsx
+++ Food-Order-App/src/store/CartProvider.jsx
@@ -1,42 +1,58 @@
 import CartContext from "./CartContext";
 import { useReducer } from "react";
 
-const defaultCartState={
-    items:[],
-    totalAmount:0
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
 };
 
-const cartReducer=(state,action)=>{
-    if(action.type==='ADD'){
-        const existingItemIndex=state.items.findIndex(item=>item.id===action.item.id)
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD') {
+        const existingItemIndex = state.items.findIndex(item => item.id === action.item.id)
 
-        const existingCartItem=state.items[existingItemIndex];
+        const existingCartItem = state.items[existingItemIndex];
         let updatedItems;
-        if(existingCartItem){
-            
-            const updatedItem={
-                ...existingCartItem,
-                amount:existingCartItem.amount+action.item.amount
-            }
-            updatedItems=[...state.items]
-            updatedItems[existingItemIndex]=updatedItem 
-        }else{
+        if (existingCartItem) {
 
-            updatedItems=state.items.concat(action.item);
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            }
+            updatedItems = [...state.items]
+            updatedItems[existingItemIndex] = updatedItem
+        } else {
+            updatedItems = state.items.concat(action.item);
         }
-        const updatedTotalAmount=state.totalAmount+action.item.price*action.item.amount
-        console.log(action.item.name);
-        return {items:updatedItems,totalAmount:updatedTotalAmount}
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+        return { items: updatedItems, totalAmount: updatedTotalAmount };
+    };
+    if (action.type === 'REMOVE') {
+        console.log(action.id);
+        const existingItemIndex = state.items.findIndex(item => item.id === action.id);
+        console.log(existingItemIndex);
+        const existingCartItem = state.items[existingItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+        let updatedItems;
+        if (existingCartItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id)
+        } else {
+            const updatedItem = { ...existingCartItem, amount: existingCartItem.amount - 1 };
+            updatedItems = [...state.items];
+            updatedItems[existingItemIndex] = updatedItem;
+        }
+        return { items: updatedItems, totalAmount: updatedTotalAmount };
+
     }
     return defaultCartState
 }
 const CartProvider = props => {
-    const [cartState,dispatchCartAction] =useReducer(cartReducer,defaultCartState)
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
     const addItemstoCartHandler = item => {
-        dispatchCartAction({type:'ADD',item:item})
+        dispatchCartAction({ type: 'ADD', item: item })
     }
-    const removeItemstoCartHandler = id => {     
-        dispatchCartAction({type:'REMOVE',id:id})
+    const removeItemstoCartHandler = id => {
+        dispatchCartAction({ type: 'REMOVE', id: id })
+        console.log(id);
     }
     const cartContext = {
         items: cartState.items,
