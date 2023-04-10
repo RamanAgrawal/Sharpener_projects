ab6e6d180ff085c3b1f090846c41d263c8e67498
RamanAgrawal <iramanagrawal@gmail.com>
2023-04-10 13:29:50 +0530

    lazy loading

--- ecommerce-site/src/App.js
+++ ecommerce-site/src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
 import Header from './components/Header';
-import Store from './components/Store';
 import Footer from './components/Footer';
-import About from './components/About';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import Cart from './components/Cart';
-import Contect from './components/Contect';
 import ProductDetails from './components/ProductDetails';
 import { AuthContext } from './context/AuthContext';
 import Login from './components/Authentication/Login';
 import Signup from './components/Authentication/Sighup';
 import Profile from './components/Authentication/Profile';
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import axios from 'axios';
 import { CartContext } from './context/CartContext';
+import React from 'react';
+import Loader from './ui/Loader';
 
+const Store = React.lazy(() => import('./components/Store'))
+const Cart = React.lazy(() => import('./components/Cart/Cart'))
+const About = React.lazy(() => import('./components/About'))
+const Contect = React.lazy(() => import('./components/Contect'))
 function App() {
   const { replaceItem } = CartContext()
   const { isLoggedIn } = AuthContext()
@@ -26,8 +28,8 @@ function App() {
         const username = email.replace('@', "").replace('.', "")
         try {
           const res = await axios(`https://my-first-project-c381c-default-rtdb.firebaseio.com/${username}.json`)
-          // console.log(res);
-          if(res.data){
+
+          if (res.data) {
             replaceItem(res.data)
           }
         } catch (error) {
@@ -40,20 +42,26 @@ function App() {
 
   return (
     <div>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/store/:productId' element={<ProductDetails />} />
-        <Route path='/store' element={<Store />} />
-        <Route path='/about' element={<About />} />
-        {isLoggedIn &&
-          <Route path='/cart' element={<Cart />} />}
-        <Route path='/contect' element={<Contect />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/profile' element={<Profile />} />
 
-      </Routes>
+      <Suspense fallback={
+        <div style={{ height: '100vh', width: '100%' }} className='d-flex justify-content-center align-items-center'>
+          <Loader />
+        </div>}>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/store/:productId' element={<ProductDetails />} />
+          <Route path='/store' element={<Store />} />
+          <Route path='/about' element={<About />} />
+          {isLoggedIn &&
+            <Route path='/cart' element={<Cart />} />}
+          <Route path='/contect' element={<Contect />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/profile' element={<Profile />} />
+
+        </Routes>
+      </Suspense>
       <Footer />
     </div>
 
